4c9c4b50257c7da4716963b0d9783f400378a8ec
Darcio Marcilio <flybydance@hotmail.com>
2021-06-10 20:03:50 -0300

    adicionei outras operações

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -6,16 +6,20 @@ public class Calculadora {
 		Scanner leitor = new Scanner (System.in);
 		double valor1;
 		double valor2;
-		double soma;
+		double soma, divisao, multiplicacao, subtracao;
 		System.out.println("Favor digite um valor:");
 		valor1 = leitor.nextDouble();
 		System.out.println("Digite mais um valor:");
 		valor2 = leitor.nextDouble();
 		
 		soma = valor1 + valor2;
-		
+		subtracao = valor1 - valor2;
+		multiplicacao = valor1 * valor2;
+		divisao = valor1 / valor2;
 		System.out.println("A soma dos dois valores informados é" + soma);
-		
+		System.out.println("A subtracao dos dois valores informados é" + subtracao);
+		System.out.println("A multiplicacao dos dois valores informados é" + multiplicacao);
+		System.out.println("A divisao dos dois valores informados é" + divisao);
 		leitor.close();
 	}
 
